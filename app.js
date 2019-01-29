bca1f5ff7eceb846933eaabec87c5070997382cd
Nico De Witte <nico.dewitte@vives.be>
2019-01-29 17:03:14 +0100

    :)

---
+++ app.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+setInterval(function(){ 
+  axios.get('http://172.16.4.71:4000?say=[todo]-never-gonna-give-you-up')
+  .then(response => {
+    // console.log(response.data);
+    // console.log(response.data.explanation);
+    // console.log(".");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+ }, 5);
+
+
